184b606697a9fee8a496cde9d6aca537b69ec239
Carlotta Cruciani <ccrucian@c2r4s3.42roma.it>
2026-09-10 12:15:59 +0200

    ok

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,15 @@
+def main():
+
+    name = "Rose"
+    height = "10"
+    age = "1"
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    main()
